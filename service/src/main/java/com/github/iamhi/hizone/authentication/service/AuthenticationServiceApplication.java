8221c8855c2d3e61ab567c2a25f38a866d846b0c
iamhi <inpactt@gmail.com>
2022-07-09 19:37:35 +0200

    Postgres databaes fix

--- service/src/main/java/com/github/iamhi/hizone/authentication/service/AuthenticationServiceApplication.java
+++ service/src/main/java/com/github/iamhi/hizone/authentication/service/AuthenticationServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.github.iamhi.hizone.authentication"})
-//@EnableJpaRepositories(basePackages = {"com.github.iamhi.hizone.authentication.data"})
 @ConfigurationPropertiesScan("com.github.iamhi.hizone.authentication.config")
 @EnableR2dbcRepositories(basePackages = {"com.github.iamhi.hizone.authentication.data"})
 public class AuthenticationServiceApplication {
